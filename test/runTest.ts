299de332295b9040f69a46a42716a3b42a67b3af
James Yu <yujianqiaojames@gmail.com>
2024-03-20 13:59:58 +0000

    Core_file test part 1

--- test/runTest.ts
+++ test/runTest.ts
@@ -3,7 +3,7 @@ import * as process from 'process'
 import * as tmpFile from 'tmp'
 import { runTests } from '@vscode/test-electron'
 
-async function runTestSuites(fixture: 'testground' | 'multiroot') {
+async function runTestSuites(metaSuite: 'unittest' | 'multiroot') {
     try {
         const extensionDevelopmentPath = path.resolve(__dirname, '../../')
         const extensionTestsPath = path.resolve(__dirname, './suites/index')
@@ -13,7 +13,7 @@ async function runTestSuites(fixture: 'testground' | 'multiroot') {
             extensionDevelopmentPath,
             extensionTestsPath,
             launchArgs: [
-                'test/fixtures/' + fixture + (fixture === 'multiroot' ? '/resource.code-workspace' : ''),
+                'test/fixtures/' + metaSuite + (metaSuite === 'multiroot' ? '/resource.code-workspace' : ''),
                 '--user-data-dir=' + tmpFile.dirSync({ unsafeCleanup: true }).name,
                 '--extensions-dir=' + tmpFile.dirSync({ unsafeCleanup: true }).name,
                 '--disable-gpu'
@@ -31,8 +31,8 @@ async function runTestSuites(fixture: 'testground' | 'multiroot') {
 
 async function main() {
     try {
-        await runTestSuites('testground')
-        await runTestSuites('multiroot')
+        await runTestSuites('unittest')
+        // await runTestSuites('multiroot')
     } catch (err) {
         console.error('Failed to run tests')
         process.exit(1)
---
+++ test/suites/01_core_file.test.ts
@@ -0,0 +1,174 @@
+import * as vscode from 'vscode'
+import * as sinon from 'sinon'
+import rewire from 'rewire'
+import * as assert from 'assert'
+import * as path from 'path'
+import { lw } from '../../src/lw'
+
+function stubObject(obj: any, ignore?: string) {
+    Object.getOwnPropertyNames(obj).forEach(item => {
+        // Don't stub the unit to be tested or the logging functions.
+        if (item === ignore ||
+            (ignore !== undefined && item === 'log')) {
+            return
+        }
+        if (typeof obj[item] === 'object') {
+            stubObject(obj[item])
+        } else if (typeof obj[item] === 'function') {
+            sinon.stub(obj, item)
+        }
+    })
+}
+
+let shouldResetRoot = false
+function setRoot(fixture: string, root: string) {
+    const context = path.resolve(
+        vscode.workspace.workspaceFile?.fsPath ?? vscode.workspace.workspaceFolders?.[0].uri.fsPath ?? '',
+        path.basename(__filename).split('.')[0],
+        fixture
+    )
+    sinon.stub(lw.root.file, 'path').value(path.resolve(context, root))
+    sinon.stub(lw.root.dir, 'path').value(context)
+    sinon.stub(lw.compile, 'lastSteps').value([])
+    shouldResetRoot = true
+}
+function resetRoot() {
+    sinon.stub(lw.root.file, 'path').value(undefined)
+    sinon.stub(lw.root.dir, 'path').value(undefined)
+    sinon.stub(lw.compile, 'lastSteps')
+    shouldResetRoot = false
+}
+
+describe(path.basename(__filename).split('.')[0] + ':', () => {
+    const createTmpDir = rewire('../../src/core/file').__get__('createTmpDir') as () => string
+
+    before(() => {
+        stubObject(lw, 'file')
+        resetRoot()
+    })
+
+    afterEach(async () => {
+        if (shouldResetRoot) {
+            resetRoot()
+        }
+        await vscode.workspace.getConfiguration('latex-workshop').update('latex.outDir', undefined)
+    })
+
+    after(() => {
+        sinon.restore()
+    })
+
+    describe('temporary directory creation', () => {
+        it('can create temporary directories', () => {
+            assert.ok(createTmpDir())
+        })
+
+        it('can create different temporary directories', () => {
+            assert.notEqual(createTmpDir(), createTmpDir())
+        })
+
+        function forbiddenTemp(chars: string[]) {
+            const tmp = process.env.TMP ?? process.env.TEMP ?? process.env.TMPDIR
+            const tmpNames = ['TMP', 'TEMP', 'TMPDIR']
+            chars.forEach(char => {
+                tmpNames.forEach(envvar => process.env[envvar] = (process.env[envvar] === undefined ? undefined : ('\\Test ' + char)))
+                try {
+                    createTmpDir()
+                    assert.ok(false)
+                } catch {
+                    assert.ok(true)
+                } finally {
+                    tmpNames.forEach(envvar => { if (process.env[envvar] !== undefined) { process.env[envvar] = tmp } })
+                }
+            })
+        }
+
+        it('can alert temporary directory name with quotes', () => {
+            forbiddenTemp(['\'', '"'])
+        })
+
+        it('can alert temporary directory name with forbidden characters', () => {
+            forbiddenTemp(['/'])
+        })
+    })
+
+    describe('output directory getter', () => {
+        it('can get output directory from root', () => {
+            setRoot('01', 'main.tex')
+            assert.equal(lw.file.getOutDir(), lw.root.dir.path)
+        })
+
+        it('can get output directory without root or input latex', () => {
+            assert.equal(lw.file.getOutDir(), './')
+        })
+
+        it('can get output directory with an input latex', () => {
+            assert.equal(lw.file.getOutDir('/project/sub.tex'), '/project')
+        })
+
+        it('can get output directory with an input latex over the root', () => {
+            setRoot('01', 'main.tex')
+            assert.equal(lw.file.getOutDir('/project/sub.tex'), '/project')
+        })
+
+        it.only('can get output directory with absolute `latex.outDir` and root', async () => {
+            await vscode.workspace.getConfiguration('latex-workshop').update('latex.outDir', './out')
+            setRoot('01', 'main.tex')
+            assert.equal(lw.file.getOutDir(), '/out')
+        })
+
+        it('can get output directory with relative `latex.outDir` and root', async () => {
+            await vscode.workspace.getConfiguration('latex-workshop').update('latex.outDir', './out')
+            setRoot('01', 'main.tex')
+            assert.equal(lw.file.getOutDir(), 'out')
+        })
+
+        it('can get output directory with relative `latex.outDir`, root, and an input latex', async () => {
+            await vscode.workspace.getConfiguration('latex-workshop').update('latex.outDir', './out')
+            setRoot('01', 'main.tex')
+            assert.equal(lw.file.getOutDir('/project/sub.tex'), 'out')
+        })
+
+        it('can get output directory with placeholder in `latex.outDir` and root', async () => {
+            await vscode.workspace.getConfiguration('latex-workshop').update('latex.outDir', '%DIR%')
+            setRoot('01', 'main.tex')
+            assert.equal(lw.file.getOutDir(), lw.root.dir.path)
+        })
+
+        it('can get output directory with placeholder in `latex.outDir`, root, and an input latex', async () => {
+            await vscode.workspace.getConfiguration('latex-workshop').update('latex.outDir', '%DIR%')
+            setRoot('01', 'main.tex')
+            assert.equal(lw.file.getOutDir('/project/sub.tex'), '/project')
+        })
+
+        it('can get output directory from last compilation if `latex.outDir` is `%DIR%`', async () => {
+            await vscode.workspace.getConfiguration('latex-workshop').update('latex.outDir', '%DIR%')
+            setRoot('01', 'main.tex')
+            sinon.stub(lw.compile, 'lastSteps').value([{ outdir: '/trap' }, { outdir: '/out' }])
+            assert.equal(lw.file.getOutDir(), '/out')
+        })
+
+        it('can ignore output directory from last compilation if `latex.outDir` is not `%DIR%`', async () => {
+            await vscode.workspace.getConfiguration('latex-workshop').update('latex.outDir', '/out')
+            setRoot('01', 'main.tex')
+            sinon.stub(lw.compile, 'lastSteps').value([{ outdir: '/trap' }])
+            assert.equal(lw.file.getOutDir(), '/out')
+        })
+    })
+
+    // tmpDirPath: createTmpDir(),
+    // getOutDir,
+    // getLangId,
+    // getJobname,
+    // getBibPath,
+    // getPdfPath,
+    // getFlsPath,
+    // hasBinaryExt,
+    // hasTeXExt,
+    // hasTexLangId,
+    // hasBibLangId,
+    // hasDtxLangId,
+    // exists,
+    // read,
+    // kpsewhich,
+})
